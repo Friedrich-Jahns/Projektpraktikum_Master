6c7168dd6a1ddc7500bc0435c21a56fb7c7967d2
Tim Lockstedt <t.lockstedt@gmx.de>
2025-12-04 16:22:50 +0100

    masked veiwer

--- code/viewer/viewer_maske.py
+++ code/viewer/viewer_maske.py
@@ -5,7 +5,6 @@ import h5py
 from pathlib import Path
 import os
 
-cwd = Path.cwd()
 path = Path('PE-2025-01953-M_00_s0060_PM_Complete_Transmittance_Stitched_Flat_v000-Image_Probabilities.h5')
 
 Filepath = Path(os.path.dirname(os.path.abspath(__file__)))
@@ -14,13 +13,14 @@ print(fullpath)
 viewer = napari.Viewer()
 
 with h5py.File(fullpath,'r') as file:
-    data = file['Image'][:]
+    data = np.array(file['exported_data'][:])
 #    img = np.zeros(data.shape) 
 #    file['Image'].read_direct(img,(np.s_[0:-1, 0:-1], np.s_[0:-1, 0:-1])) 
 
 
 
 
-viewer.add_image(data, name="Mein H5-Bild")
-
+viewer.add_image(data[:,:,0], name="Maske 1")
+viewer.add_image(data[:,:,1], name="Maske 2")
+viewer.add_image(data[:,:,2], name="Maske 3")
 napari.run()
