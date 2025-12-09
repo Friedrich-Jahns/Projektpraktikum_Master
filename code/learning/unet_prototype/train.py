5decb5937e54920d73b814ceb55996c924399aed
Friedrich-Jahns <jahnsfriedrich@gmail.com>
2025-12-09 15:05:32 +0100

    working unet_prototype

---
+++ code/learning/unet_prototype/train.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+from dataset import get_dataloader
+from unet import UNet
+from pathlib import Path
+
+# Pfade zu deinen Daten
+dat_path = Path.cwd().parent.parent.parent / "data" / "training" /'train'
+
+
+image_dir = dat_path / "img"
+mask_dir = dat_path / "mask"
+
+# DataLoader
+dataloader = get_dataloader(image_dir, mask_dir, batch_size=4)
+
+# Gerät
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Modell, Loss, Optimizer
+model = UNet().to(device)
+criterion = nn.BCELoss()  # binäre Segmentierung
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+# Training
+num_epochs = 10
+for epoch in range(num_epochs):
+    model.train()
+    for images, masks in dataloader:
+        images = images.to(device)
+        masks = masks.to(device).float()
+
+        outputs = model(images)
+        loss = criterion(outputs, masks)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item():.4f}")
+
+# Modell speichern
+torch.save(model.state_dict(), "unet_model.pth")
