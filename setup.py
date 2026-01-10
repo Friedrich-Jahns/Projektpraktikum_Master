ed2231c92e256fed028ef3a388a06ef76cf6d751
Friedrich-Jahns <jahnsfriedrich@gmail.com>
2026-01-10 11:20:21 +0100

    littel change in setupskript

--- setup.py
+++ setup.py
@@ -7,9 +7,11 @@ folders = [
     "data/training/train/mask",
     "data/training/val/img",
     "data/training/val/mask",
+    'data/ilastik',
     'data/raw/img',
-    'data/raw/mask',
+    'data/raw/vessel_mask',
     'data/raw/bgr_mask'
+
 
 ]
 
@@ -21,13 +23,14 @@ for folder in folders:
     Path(folder).mkdir(parents=True, exist_ok=True)
 print(dat_path)
 
-for i,path in enumerate(Path.iterdir(dat_path)):
-    if any([i=='v000' or i=='v000-Image' for i in str(path.stem).split('_')]):
-        if path.suffix == '.h5':
-            if any([i=='Probabilities' for i in str(path.stem).split('_')]):
-                type = 'mask'
-                if any([i=='background' for i in str(path.stem).split('_')]):
-                    type = 'bgr_mask'
-            else:
-                type = 'img'
-            os.rename(path,target_path/type/path.name)
+if True:
+    for j,path in enumerate(Path.iterdir(dat_path)):
+        if any([i=='v000' or i=='v000-Image' for i in str(path.stem).split('_')]):
+            if path.suffix == '.h5':
+                if any([i=='Probabilities' for i in str(path.stem).split('_')]):
+                    type = 'vessel_mask'
+                    if any([i=='background' for i in str(path.stem).split('_')]):
+                        type = 'bgr_mask'
+                else:
+                    type = 'img'
+                os.rename(path,target_path/type/path.name)
