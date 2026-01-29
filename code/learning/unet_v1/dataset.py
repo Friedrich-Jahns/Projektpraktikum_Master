89fc8d9dd400965a9e995174dba8cb25afa605c8
Friedrich-Jahns <jahnsfriedrich@gmail.com>
2026-01-29 11:11:39 +0100

    unet_v1 validation,possibility to randmly susample train and val data and trainingplot implemented

--- code/learning/unet_v1/dataset.py
+++ code/learning/unet_v1/dataset.py
@@ -4,16 +4,24 @@ from PIL import Image
 import torch
 from torch.utils.data import DataLoader
 import numpy as np
+import random
 
 class img_dataset(Dataset):
-    def __init__(self,img_dir,mask_dir,transform=None):
+    def __init__(self,img_dir,mask_dir,transform=None,max_samples=None,seed=42):
         self.img_dir = img_dir
         self.mask_dir = mask_dir
         self.transform = transform
 
         self.images = sorted(os.listdir(img_dir))
         self.masks = sorted(os.listdir(mask_dir))
-    
+        
+        if max_samples is not None:
+            random.seed(seed)
+            indices = random.sample(range(len(self.images)), max_samples)
+
+            self.images = [self.images[i] for i in indices]
+            self.masks  = [self.masks[i]  for i in indices]
+
     def __len__(self):
         return len(self.images)
 
@@ -30,13 +38,13 @@ class img_dataset(Dataset):
         mask = (mask>.5).float()
 
         if self.transform:
-            img,mask = self.transform(img,mask)
+           img,mask = self.transform(img,mask)
             
         return img, mask
 
 
-def dataloader(img_dir,mask_dir,bs=4,shuffle=True,num_workers=0):
-    dataset = img_dataset(img_dir,mask_dir)
+def dataloader(img_dir,mask_dir,bs=4,shuffle=True,num_workers=0,max_samples=None):
+    dataset = img_dataset(img_dir,mask_dir,max_samples=max_samples)
     dataloader = DataLoader(
     dataset,
     batch_size = bs,
--- code/learning/unet_v1/train.py
+++ code/learning/unet_v1/train.py
@@ -3,27 +3,35 @@ from torch import nn,optim
 from unet import Unet,dice_loss
 from dataset import dataloader
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+import numpy as np
 
-batch_size = 4
+batch_size = 1
 learning_rate = 1e-3
-epochs = 10
+epochs = 5
 
 device = torch.device(cuda if torch.cuda.is_available() else 'cpu')
 
 img_path = '/home/friedrich/Dokumente/Master/PP/Projektpraktikum_Master/data/training/train/img'
 mask_path = '/home/friedrich/Dokumente/Master/PP/Projektpraktikum_Master/data/training/train/mask'
 
-dataloader = dataloader(img_path,mask_path,bs=batch_size,shuffle=True)
+
+img_val_path = '/home/friedrich/Dokumente/Master/PP/Projektpraktikum_Master/data/training_mini/val/img'
+mask_val_path = '/home/friedrich/Dokumente/Master/PP/Projektpraktikum_Master/data/training_mini/val/mask'
+
+train_dataloader = dataloader(img_path,mask_path,bs=batch_size,shuffle=True,max_samples=20)
+val_dataloader = dataloader(img_val_path,mask_val_path,bs=batch_size,shuffle=True,max_samples=4) 
 
 model = Unet().to(device)
 
 optimizer = optim.Adam(model.parameters(),lr=learning_rate)
 
+train_log = []
+
 for epoch in tqdm(range(epochs)):
     model.train()
     epoch_loss = 0.0
-    loss_log = []
-    for imgs,masks in tqdm(dataloader):
+    for imgs,masks in tqdm(train_dataloader):
         imgs = imgs.to(device)
         mask = masks.to(device) 
         
@@ -34,10 +42,28 @@ for epoch in tqdm(range(epochs)):
         optimizer.step()
 
         
-        loss_log.append(loss)
         epoch_loss += loss.item()
+    epoch_loss/=len(train_dataloader)
 
-    print(f'{epoch+1}/{num_epochs},loss:{epoch_loss/len(dataloader):.4f}')
+    model.eval()
+    validation_loss = 0.0
+
+    with torch.no_grad():
+        for val_imgs, val_masks in tqdm(val_dataloader):
+            val_imgs = val_imgs.to(device)
+            val_masks = val_masks.to(device)
+
+            val_output = model(val_imgs)
+            val_loss = dice_loss(val_output,val_masks)
+            validation_loss += val_loss.item()
+        validation_loss/=len(val_dataloader)
+    train_log.append([epoch_loss,validation_loss])
+    print(f'{epoch+1}/{epochs},trainloss:{epoch_loss:.4f},val_loss{validation_loss:.4f}')
 torch.save(model.state_dict(), "unet_model.pth")
 
+train_log = np.array(train_log).T
+plt.plot(train_log[0],label='train_loss')
+plt.plot(train_log[1],label='val_loss')
+plt.legend()
+plt.savefig('train_log')
 
