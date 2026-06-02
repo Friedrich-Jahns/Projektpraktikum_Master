7943f38cb17ff2fcf9e9b3d8210d55550cee7b6d
Tim Lockstedt <t.lockstedt@gmx.de>
2026-06-02 14:54:56 +0200

    ca 8 sekunden pro Epoche ROCm

--- augmentation_testing/run_test.py
+++ augmentation_testing/run_test.py
@@ -26,19 +26,18 @@ def main():
     parser.add_argument("--run_name",     type=str, required=True)
     parser.add_argument("--epochs",       type=int, default=100)
     parser.add_argument("--bs",           type=int, default=16)
-    parser.add_argument("--lr",           type=float, default=1e-2)
-    parser.add_argument("--worker",           type=float, default=1)
+    parser.add_argument("--lr",           type=float, default=1e-3)
+    parser.add_argument("--worker",           type=int, default=1)
     args = parser.parse_args()
-
+    # batches vorab laden
+    prefetch_factor = 32   # Batches vorab laden
+    
     cwd = Path(os.getcwd()).parent.parent
     img_path      = cwd / 'Projektpraktikum_Master/augmentation_testing/dat/train/img'
     mask_path     = cwd / 'Projektpraktikum_Master/augmentation_testing/dat/train/mask'
     img_val_path  = cwd / 'Projektpraktikum_Master/augmentation_testing/dat/val/img'
     mask_val_path = cwd / 'Projektpraktikum_Master/augmentation_testing/dat/val/mask'
-    # batches vorab laden
-    prefetch_factor = 32   # Batches vorab laden
-    load_time = 0
-    train_time = 0
+
     out_dir = Path("res") / args.run_name
     out_dir.mkdir(parents=True, exist_ok=True)
 
@@ -48,6 +47,8 @@ def main():
         # natürlich immernoch deutlich langsamer als cuda wenn es funktionieren würde D:
         import torch_directml
         device = torch_directml.device()
+    
+    use_amp = device.type == "cuda"
     aug = load_augmentation(args.augmentation, ref_dir=img_val_path)
 
     train_dataloader = dataloader(img_path, mask_path, transform=aug, bs=args.bs, shuffle=True, max_samples=50, num_workers=args.worker, prefetch = prefetch_factor)
@@ -55,7 +56,7 @@ def main():
 
     model     = Unet().to(device)
     model = model.to(memory_format=torch.channels_last)
-    model = torch.compile(model)  # einmalig vor dem Training (Test)
+    model = torch.compile(model)  # einmalig vor dem Training (Test) ,mode="reduce-overhead"
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
     criterion = torch.nn.BCEWithLogitsLoss()
     # torch.backends.cudnn.benchmark = True
@@ -65,9 +66,7 @@ def main():
 
     train_log = []
     best_val_loss = float('inf')
-    # New
-    scaler = GradScaler()
-    #accumulation_steps = 8 #Try
+    scaler = GradScaler(enabled=use_amp)
 
     optimizer.zero_grad(set_to_none=True)
     for epoch in tqdm(range(args.epochs)):
@@ -75,45 +74,29 @@ def main():
         epoch_loss = 0.0
 
         for i, batch in tqdm(enumerate(train_dataloader), leave=False):
-            t0 = time.time()
-            images, masks = batch
-            images, masks = images.to(device, memory_format=torch.channels_last), masks.to(device, memory_format=torch.channels_last)
-            #optimizer.zero_grad()
-            t1 = time.time()
-            print(images.shape)
 
-            
-            with autocast("cuda"):
+            images, masks = batch
+            images, masks = images.to(device, non_blocking=True, memory_format=torch.channels_last), masks.to(device, non_blocking=True,)
+            with autocast(device_type=device.type, enabled=use_amp):
                 output =  model(images) # torch.sigmoid(model(imgs))
-                loss = criterion(output, masks) # criterion(outputs, masks) + dice_loss(outputs, masks)
+                loss = criterion(output, masks) + dice_loss(output, masks)# criterion(outputs, masks) + dice_loss(outputs, masks)
             scaler.scale(loss).backward()
-            # nur alle accumulation_steps batches updaten -> simuliert größere Batches
-            # if (i + 1) % accumulation_steps == 0:
-            #     scaler.step(optimizer)
-            #     scaler.update()
-            #     optimizer.zero_grad(set_to_none=True)
             scaler.step(optimizer)
             scaler.update()
             optimizer.zero_grad(set_to_none=True)
-            # t2 = time.time()
-            # load_time  += (t1 - t0)
-            # train_time += (t2 - t1)
-            # loss.backward()
-            # optimizer.step()
             epoch_loss += loss.item()
         epoch_loss /= len(train_dataloader)
-        # print(f"Datenladen: {load_time:.2f}s")
-        # print(f"Training:   {train_time:.2f}s")
-        # total = sum(p.numel() for p in model.parameters())
-        # print(f"Parameter: {total:,}")
-
         model.eval()
         val_loss = 0.0
         with torch.no_grad():
             for val_imgs, val_masks in tqdm(val_dataloader, leave=False):
-                val_imgs, val_masks = val_imgs.to(device), val_masks.to(device)
-                val_outputs = model(val_imgs)
-                loss_v = criterion(val_outputs, val_masks) + dice_loss(val_outputs, val_masks)
+                val_imgs, val_masks = val_imgs.to(device, non_blocking=True, memory_format=torch.channels_last), val_masks.to(device, non_blocking=True)
+                with autocast(device_type=device.type, enabled=use_amp):
+                    val_outputs = model(val_imgs)
+                    loss_v = (
+                        criterion(val_outputs, val_masks)
+                        + dice_loss(val_outputs, val_masks)
+                    )
                 val_loss += loss_v.item()
         val_loss /= len(val_dataloader)
 
@@ -124,16 +107,17 @@ def main():
             torch.save(model.state_dict(), out_dir / "best_model.pth")
 
         log_arr = np.array(train_log).T
-        plt.plot(log_arr[0], label='train_loss')
-        plt.plot(log_arr[1], label='val_loss')
-        plt.legend()
-        plt.title(f'{epoch+1}/{args.epochs} | aug: {args.augmentation}')
-        plt.savefig(out_dir / 'train_log.png')
-        plt.clf()
+        #plt.plot(log_arr[0], label='train_loss')
+        #plt.plot(log_arr[1], label='val_loss')
+        #plt.legend()
+        #plt.title(f'{epoch+1}/{args.epochs} | aug: {args.augmentation}')
+        #plt.savefig(out_dir / 'train_log.png')
+        #plt.clf()
 
         print(f'{epoch+1}/{args.epochs} | train: {epoch_loss:.4f} | val: {val_loss:.4f}')
-
-    torch.save(model.state_dict(), out_dir / "last_model.pth")
+                    # _orig_mod ist wegen dem torch.compile(model) ein präfix der den parametern hinzugefügt wird 
+                    # und beim einlesen der parameter stört
+    torch.save(model._orig_mod.state_dict(), out_dir / "last_model.pth")
     # np.save(out_dir / "train_log.npy", np.array(train_log))
     log_arr = np.array(train_log).T
     with open(out_dir / "train_log.json", "w") as f:
@@ -141,7 +125,12 @@ def main():
             "train_loss": log_arr[0].tolist(),
             "val_loss":   log_arr[1].tolist()
         }, f, indent=2)
-
+    plt.plot(range(1, args.epochs + 1), log_arr[0], label='train_loss')
+    plt.plot(range(1, args.epochs + 1), log_arr[1], label='val_loss')
+    plt.legend()
+    plt.title(f'{epoch+1}/{args.epochs} | aug: {args.augmentation}')
+    plt.savefig(out_dir / 'train_log.png')
+    plt.clf()
 
 if __name__ == "__main__":
     main()
